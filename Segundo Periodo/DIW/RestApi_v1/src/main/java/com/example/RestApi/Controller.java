02119aadba0bf3b61d200b7bf212465b1354bb37
missacoder <fernando.leitepagani@protonmail.com>
2026-08-14 08:33:37 -0300

    DIW

---
+++ Segundo Periodo/DIW/RestApi_v1/src/main/java/com/example/RestApi/Controller.java
@@ -0,0 +1,18 @@
+package com.example.RestApi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/test")
+    public String helloword(){
+        return "Essa é minha primeira API REST";
+    }
+    @GetMapping("/diaw")
+	public String diaw() {
+        return "Esse é meu segundo endpoint";
+	}
+
+}
