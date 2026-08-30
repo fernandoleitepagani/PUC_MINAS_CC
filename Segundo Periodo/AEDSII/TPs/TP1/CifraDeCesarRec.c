910f14d834052b8b9f28eeabaee4d82b976ebe20
fernandoleitepagani <fernando.leitepagani@protonmail.com>
2026-08-30 20:20:54 -0300

    remove one line break from Cifra Recursiva

--- Segundo Periodo/AEDSII/TPs/TP1/CifraDeCesarRec.c
+++ Segundo Periodo/AEDSII/TPs/TP1/CifraDeCesarRec.c
@@ -22,22 +22,24 @@ char* cifrar(char *s){
 }
 
 int main(){
-  int length = 0;
-  char passwd[MAX];
-  char scanned;
-  while (1) {
-    length = 0;
-
-    // lê caractere por caractere até o '\n' 
-    while (scanf("%c", &scanned) == 1) {
-      if (scanned == '\n') break;
-      if (length < MAX - 1) { passwd[length] = scanned; length++; }
+    int length = 0;
+    char passwd[MAX];
+    char scanned;
+    while (1) {
+        length = 0;
+        // lê caractere por caractere até o '\n'
+        while (scanf("%c", &scanned) == 1 && scanned != '\n') {
+            if (length < MAX - 1) { 
+                passwd[length] = scanned; 
+                length++; 
+            }
+        }
+        passwd[length] = '\0'; // fim da string
+        // ser ler FIM, para
+        if (passwd[0] == 'F' && passwd[1] == 'I' && passwd[2] == 'M' && passwd[3] == '\0') 
+            return 0;
+        printf("%s\n", cifrar(passwd));
     }
-    passwd[length] = '\0'; //fim da string
 
-    // ser ler FIM, para
-    if (passwd[0] == 'F' && passwd[1] == 'I' && passwd[2] == 'M' && passwd[3] == '\0') return 0;
-    printf("%s\n", cifrar(passwd));
-  }
-  return 0;
+    return 0;
 }
