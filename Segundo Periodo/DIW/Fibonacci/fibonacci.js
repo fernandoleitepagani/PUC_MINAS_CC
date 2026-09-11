18992e8f342eb53062e1a0cf4d3bd4a1a7d6bdb4
fernandoleitepagani <fernando.leitepagani@protonmail.com>
2026-09-11 08:20:48 -0300

    .

---
+++ Segundo Periodo/DIW/Fibonacci/fibonacci.js
@@ -0,0 +1,27 @@
+var procedimento = "";
+
+function fibonacci(n) {
+  if (n==0){
+    procedimento += "Fibonacci(0) = 0";
+    return (0);
+  }
+  else if (n==1){
+    procedimento += "Fibonacci(1) = 1";
+    return (1);
+  }
+  else{
+    var x=n-1;
+    var y=n-2;
+    procedimento += "Fibonacci("+n+") = Fibonacci("+x+") + Fibonacci("+y+")";
+    return (fibonacci(n-1) + fibonacci(n-2));
+  }
+}
+
+function fib(form) {
+  form.resultado.value = fibonacci(form.texto.value);
+  meuform.txtarea.value = procedimento;
+}
+
+function limpar(){
+  meuform.texto.value = "";
+}
